8b9cf3f8b73a1fbb625e36e2b3d44880c00d70d9
lin0211 <slinj0211@gmail.com>
2023-11-10 23:26:38 +0900

    form design done, js funxtion add for email valid and password visiable

---
+++ mission-02/mission-02.js
@@ -0,0 +1,25 @@
+function emailCheck(input) {
+  const email = input;
+  if (
+    email.match(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    )
+  ) {
+    document.getElementById("email-invalid").style.visibility = "hidden";
+  } else {
+    document.getElementById("email-invalid").style.visibility = "visible";
+  }
+}
+
+function showPwd() {
+  const pwd = document.getElementById("input-password");
+  if (pwd.type === "password") {
+    pwd.type = "text";
+    document.getElementById("show-pwd").style.backgroundImage =
+      "url(./images/아이콘/눈1.svg)";
+  } else {
+    pwd.type = "password";
+    document.getElementById("show-pwd").style.backgroundImage =
+      "url(./images/아이콘/눈2.svg)";
+  }
+}
